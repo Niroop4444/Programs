b7bf2de93f4e9363ebcce0f382ceaec6405544d3
niroopn <niroopvs@gmail.com>
2025-05-05 16:15:44 +0530

    gfg - reverse array

---
+++ gfg_challenge/reverse_array.js
@@ -0,0 +1,12 @@
+/*
+You are given an array of integers arr[]. Your task is to reverse the given array.
+
+Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+*/
+
+class Solution {
+    reverseArray(arr) {
+        return arr.reverse();
+    }
+}
