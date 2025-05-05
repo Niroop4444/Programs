96f52b7f32fad657d9d04c25570e263f0968f9b7
niroopn <niroopvs@gmail.com>
2025-05-05 16:11:43 +0530

    leet code - longest substring without repeating characters

--- leet_code/longest_substring_without_repeating_characters.dart
+++ leet_code/longest_substring_without_repeating_characters.dart
@@ -3,7 +3,14 @@ Longest Substring Without Repeating Characters
 Given a string s, find the length of the longest substring without duplicate 
 characters.
 
+Given a string s, find the length of the longest substring without duplicate characters.
+Input: s = "abcabcbb"
+Output: 3
 
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
 class Solution {
