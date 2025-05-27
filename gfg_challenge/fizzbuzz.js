6069f669a8ca014d745bb3eab592dce7933c1761
niroopn <niroopvs@gmail.com>
2025-05-27 11:54:52 +0530

    gfg - fizzbuzz using while

--- gfg_challenge/fizzbuzz.js
+++ gfg_challenge/fizzbuzz.js
@@ -17,3 +17,28 @@ function fizzBuzz(number) {
 }
 
 fizzBuzz(75);
+
+fizzBuzzUsingWhile(75);
+
+function fizzBuzzUsingWhile(number) {
+
+    const output = [];
+    let i = 1;
+
+    while (i < number) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            output.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            output.push("Fizz");
+        } else if (i % 5 === 0) {
+            output.push("Buzz");
+        } else {
+            output.push(i);
+        }
+        
+        i++;
+    }
+
+    console.log("Output using while "+output);
+    return output;
+}
