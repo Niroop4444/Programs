5a2df5ae2e59d353d405377679a5a3b79fdf479e
niroopn <niroopvs@gmail.com>
2025-10-06 19:02:27 +0530

    second talent - FizzBuzz

---
+++ second_talent/fizz_buzz.dart
@@ -0,0 +1,16 @@
+void main() {
+  int fizzBuzzNumber = 10;
+  print("$fizzBuzzNumber: ${fizzBuzz(fizzBuzzNumber)}");
+}
+
+String fizzBuzz(int number) {
+  if (number % 3 == 0 && number % 5 == 0) {
+    return "FizzBuzz";
+  } else if (number % 3 == 0) {
+    return "Fizz";
+  } else if (number % 5 == 0) {
+    return "Buzz";
+  } else {
+    return number.toString();
+  }
+}
