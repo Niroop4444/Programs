469fbf032e69b7df3d509350dfed53d4171e4e2c
niroopn <niroopvs@gmail.com>
2025-10-06 18:49:19 +0530

    second talent - largest in list

--- second_talent/largest_in_list.dart
+++ second_talent/largest_in_list.dart
@@ -1,8 +1,20 @@
 void main() {
   List<int> numbers = [11, 52, 33, 54, 51];
-  print("The largest number in the list $numbers is ${largestNumber(numbers)}");
+  print(
+    "The largest number in the list $numbers is ${largestNumber(numbers)} and using sort ${largestUsingSort(numbers)} and second largest is ${secondLargest(numbers)}",
+  );
 }
 
 int largestNumber(List<int> numbers) {
   return numbers.reduce((a, b) => a > b ? a : b);
+}
+
+int largestUsingSort(List<int> numbers) {
+  numbers.sort();
+  return numbers.last;
+}
+
+int secondLargest(List<int> numbers) {
+  numbers.sort();
+  return numbers[numbers.length - 2];
 }
