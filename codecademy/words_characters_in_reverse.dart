ef8b39194c6d83b7e2dd7b377fb71ae42b1f1bf7
niroopn <niroopvs@gmail.com>
2025-10-08 12:14:05 +0530

    codecademy - words characters in reverse

--- codecademy/words_characters_in_reverse.dart
+++ codecademy/words_characters_in_reverse.dart
@@ -1,10 +1,17 @@
 void main() {
   String sentence = "hello world dart programming";
   print("The reversed word of $sentence is : ${reverseSentence(sentence)}");
+  print(
+    "The reversed characters of $sentence is : ${reverseCharacters(sentence)}",
+  );
 }
 
 String reverseSentence(String sentence) {
-  List<String> words = sentence.trim().split(RegExp(r'\s+)'));
+  List<String> words = sentence.trim().split(RegExp(r'\s+'));
   List<String> reverseWords = words.reversed.toList();
   return reverseWords.join(' ');
+}
+
+String reverseCharacters(String sentence) {
+  return sentence.split('').reversed.join();
 }
