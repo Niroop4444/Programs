a6cc5189d7cdb2bceba78ccaf9ac66f6cc1d12fb
Niroop4444 <niroopvs@gmail.com>
2025-11-21 14:05:06 +0530

    pynative - sum of current & previous numbers

---
+++ pynative_exercises/sum_of_current_previous_number.py
@@ -0,0 +1,11 @@
+# Python code to iterate through the first 10 numbers and, 
+# in each iteration, print the sum of the current and 
+# previous number.
+
+print("Printing current and previous number and their sum in a range(10)")
+previous_num = 0
+
+for i in range(10):
+    x_sum = previous_num + i
+    print(f"Current number: {i}, Previous number: {previous_num} & Sum: {x_sum}")
+    previous_num = i
