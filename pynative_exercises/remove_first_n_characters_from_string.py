18e5175ceee291c260d98806e63be221a7f078bf
Niroop4444 <niroopvs@gmail.com>
2025-11-21 14:15:10 +0530

    pynative - remove first n characters from string

---
+++ pynative_exercises/remove_first_n_characters_from_string.py
@@ -0,0 +1,11 @@
+# Python code to remove characters from a string from 0 to n 
+# and return a new string.
+
+def remove_chars(word, n):
+    return word[n:]
+
+print("Removing characters from a string")
+print(remove_chars("pynative", 4)) 
+# output 'tive' first four characters are removed
+
+print(remove_chars("pynative", 2)) 
