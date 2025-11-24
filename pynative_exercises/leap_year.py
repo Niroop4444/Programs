314e90e240c7004e2bfb6717cabaacb5cc7c843d
niroopn <niroopvs@gmail.com>
2025-11-24 13:04:38 +0530

    pynative - leap year

---
+++ pynative_exercises/leap_year.py
@@ -0,0 +1,17 @@
+# Python code to check if the given year is a leap year
+
+def isLeapYear(year):
+    if year %4 == 0:
+        if year %100 == 0:
+            if year %400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+year = int(input("Enter a year: "))
+
+print(isLeapYear(year))
