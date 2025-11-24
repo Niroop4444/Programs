9e48896c33d62b5cdf50290cb11539792f83faef
niroopn <niroopvs@gmail.com>
2025-11-24 09:24:07 +0530

    pynative - number of occurances of a substring

--- pynative_exercises/occurances_of_substring.py
+++ pynative_exercises/occurances_of_substring.py
@@ -1,5 +1,7 @@
+# Python code to find how often the substring “Emma” appears in the given string. 
+
 def occurances_of_substring(str1, substr):
-   occurances = str1.lower().count(substr)
+   occurances = str1.lower().count(substr.lower())
    return occurances
 
-print(occurances_of_substring("The quick brown fox jumps over the lazy dog", "the"))
+print(occurances_of_substring("Emma is good developer. Emma is a writer", "Emma"))
