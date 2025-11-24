04014c10e61a0dd3338b5a4cd083f57b5355238b
niroopn <niroopvs@gmail.com>
2025-11-24 15:35:36 +0530

    pynative - accept numbers and get product

---
+++ pynative_exercises/accept_numbers.py
@@ -0,0 +1,7 @@
+# Accept a list of numbers from the user and print the product of the numbers.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = num1 * num2
+print("The product of", num1, "and", num2, "is", result)
