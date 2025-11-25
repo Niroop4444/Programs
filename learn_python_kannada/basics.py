fa2f2ae9901d2d4d860f9a8c8779e2836b1c1817
niroopn <niroopvs@gmail.com>
2025-11-25 11:53:51 +0530

    learn python kannada - basics

---
+++ learn_python_kannada/basics.py
@@ -0,0 +1,22 @@
+# Arithmetic Practice: Write a Python program that performs basic 
+# arithmetic operations (addition, subtraction, multiplication, and division) 
+# on two numbers. Define the two numbers as variables within the code 
+# and print the results for each operation.
+
+num1 = 10
+num2 = 20
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+# Swap Two Variables: Write a Python program that swaps the values of 
+# two variables with and without using a third variable.
+
+a = 10
+b = 20
+a = a + b
+b = a - b
+a = a - b
+print(a)
+print(b)
